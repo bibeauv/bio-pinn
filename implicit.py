66fb610a64d0d1398b30e27ce73cf92aa90db1bf
bibeauv <valerie.bibeau@polymtl.ca>
2021-11-03 17:32:06 -0400

    Implicit methods

--- implicit.py
+++ implicit.py
@@ -17,17 +17,19 @@ T = [1000]
 A = 5
 E = 1000
 dt = 0.0001
-tf = [0.0, 0.1, 0.2, 0.3, 0.4]
+tf = list(np.linspace(0,3,num=5))
 y0 = 1
 # x1 : Temperature
 # x2 : Reaction time
 yA = ode.analytical_solution(T, A, E, tf, y0)
 y_t = []
 y_tdt = []
-X = []
+x_dt = []
+x_t = []
 for i in range(len(tf)):
         for j in range(i+1, len(tf)):
-                X.append(tf[j]-tf[i])
+                x_dt.append(tf[j]-tf[i])
+                x_t.append(tf[i])
                 y_t.append(yA[i])
                 y_tdt.append(yA[j])
 yA_t = np.array(y_t).reshape(-1,1)
@@ -35,34 +37,42 @@ yA_tdt = np.array(y_tdt).reshape(-1,1)
 
 # Normalization
 scaler = Normalization()
-x, scaler_x = scaler.minmax(X)
+x_dt, scaler_x_dt = scaler.minmax(x_dt)
+x_t, scaler_x_t = scaler.minmax(x_t)
 
 # PINN
 dt = sn.Variable()
-cA_t = sn.Functional("cA_t", [dt], 4*[10], activation="tanh")
-cA_tdt = sn.Functional("cA_tdt", [dt], 4*[10], activation="tanh")
+t = sn.Variable()
+cA_t = sn.Functional("cA_t", [dt, t], 4*[10], activation="tanh")
+cA_tdt = sn.Functional("cA_tdt", [dt, t], 4*[10], activation="tanh")
 # Parameters
-k = sn.Parameter(0.0, inputs=[dt])
+k = sn.Parameter(0.0, inputs=[dt, t])
 # ODE
-L1 = cA_tdt*(1+k*dt) - cA_t
+L1 = sn.math.diff(cA_tdt, dt) - (-k*cA_tdt)
 # Supervised learning with data
 DATA1 = cA_t
 DATA2 = cA_tdt
 # Model
-m = sn.SciModel([dt],
+m = sn.SciModel([dt, t],
                 [L1, DATA1, DATA2], optimizer="adagrad")
-m.train([np.array(x)],
+m.train([np.array(x_dt), np.array(x_t)],
         ["zeros", yA_t, yA_tdt],
-        epochs=5000,
+        epochs=10000,
         learning_rate=0.1)
 # Predict
-tf_pred = [0.12, 0.24, 0.28, 0.35, 0.5]
-y_pred = cA_t.eval(m, [scaler_x.transform(np.array(tf_pred).reshape(-1,1))])
-y_true = ode.analytical_solution(T, A, E, tf_pred, y0)
+tf_pred = [0.05, 0.12, 0.24, 0.28, 0.35, 0.5, 0.65, 0.72, 0.89, 1.5, 1.6, 2.4, 3.6]
+x_dt = []
+x_t = []
+for i in range(len(tf_pred)-1):
+        x_dt.append(tf_pred[i+1]-tf_pred[i])
+        x_t.append(tf_pred[i+1])
+y_pred = cA_t.eval(m, [scaler_x_dt.transform(np.array(x_dt).reshape(-1,1)),
+                       scaler_x_t.transform(np.array(x_t).reshape(-1,1))])
+y_true = ode.analytical_solution(T, A, E, x_t, y0)
 print(r2_score(y_pred, y_true))
 # Visualization
-plt.plot(tf_pred, y_true)
-plt.plot(tf_pred, y_pred, 'o')
+plt.plot(x_t, y_true)
+plt.plot(x_t, y_pred, 'o')
 plt.legend(['Analytical Solution', 'Predictions'])
 plt.xlabel('Time')
 plt.ylabel('Concentration of A')
