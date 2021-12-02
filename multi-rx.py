dd35bf14ddaf6b5084c6d84b2afafbb5e4d00181
bibeauv <valerie.bibeau@polymtl.ca>
2021-12-02 08:20:01 -0500

    Multi rx working :)

--- multi-rx.py
+++ multi-rx.py
@@ -21,19 +21,20 @@ def get_features(y, pos):
 ode = ODE(2)
 k = [1.5, 2]
 dt = 0.0001
-tf = [0, 0.1, 0.2, 0.3, 0.4]
+tf = list(np.linspace(0,3,num=100))
 y0 = 1
 y, X = ode.euler_explicite_multi(k, dt, tf, y0)
-#yA = get_features(y, 0)
-#yA = np.array(yA).reshape(-1,1)
-#yB = get_features(y, 1)
-#yB = np.array(yB).reshape(-1,1)
+yA = get_features(y, 0)
+yA = np.array(yA).reshape(-1,1)
+yB = get_features(y, 1)
+yB = np.array(yB).reshape(-1,1)
 yC = get_features(y, 2)
 yC = np.array(yC).reshape(-1,1)
 
 # Normalization
 scaler = Normalization()
-x, scaler_x = scaler.minmax(X)
+#x, scaler_x = scaler.minmax(X)
+x = np.array(tf).reshape(-1,1)
 
 # PINN
 t = sn.Variable()
@@ -47,30 +48,28 @@ k2 = sn.Parameter(1.0, inputs=[t])
 L1 = sn.math.diff(cA, t) + k1*cA
 L2 = sn.math.diff(cB, t) - k1*cA + k2*cB
 L3 = sn.math.diff(cC, t) - k2*cB
-# Supervised learning with data
-DATA1 = (1-sn.utils.sign(t - 0.0001)) * (cA - 1)
-DATA2 = (1-sn.utils.sign(t - 0.0001)) * (cB - 0)
-DATA3 = cC
 # Model
 m = sn.SciModel([t],
                 [L1, L2, L3, 
-                 DATA1, 
-                 DATA2, 
-                 DATA3], optimizer="adagrad")
-m.train([np.array(x)],
+                 cA, 
+                 cB, 
+                 cC], optimizer="adagrad")
+m.train([x],
         ["zeros", "zeros", "zeros", 
-         #np.array(yA), 
-         "zeros", 
-         #np.array(yB), 
-         "zeros", 
-         np.array(yC)],
-        epochs=10000,
+         (np.arange(0,15).reshape(-1,1), yA[0:15]),
+         (np.arange(0,15).reshape(-1,1), yB[0:15]), 
+         (np.arange(0,15).reshape(-1,1), yC[0:15])],
+        epochs=100000,
+        batch_size=100,
         learning_rate=0.1)
 # Predict
-tf_pred = [0.12, 0.24, 0.28, 0.35, 0.5]
-yA_pred = cA.eval(m, [scaler_x.transform(np.array(tf_pred).reshape(-1,1))])
-yB_pred = cB.eval(m, [scaler_x.transform(np.array(tf_pred).reshape(-1,1))])
-yC_pred = cC.eval(m, [scaler_x.transform(np.array(tf_pred).reshape(-1,1))])
+tf_pred = list(np.linspace(0,3,num=10))
+yA_pred = cA.eval(m, [#scaler_x.transform(np.array(tf_pred).reshape(-1,1))])
+                      np.array(tf_pred).reshape(-1,1)])
+yB_pred = cB.eval(m, [#scaler_x.transform(np.array(tf_pred).reshape(-1,1))])
+                      np.array(tf_pred).reshape(-1,1)])
+yC_pred = cC.eval(m, [#scaler_x.transform(np.array(tf_pred).reshape(-1,1))])
+                      np.array(tf_pred).reshape(-1,1)])
 ode_pred = ODE(2)
 y_true, _ = ode_pred.euler_explicite_multi(k, dt, tf_pred, y0)
 yA_true = get_features(y_true, 0)
@@ -79,10 +78,10 @@ yC_true = get_features(y_true, 2)
 # Rate constants
 k1_pred = k1.eval(m, [np.array(x)])
 k2_pred = k2.eval(m, [np.array(x)])
+#print(k1_pred/scaler_x.inverse_transform(np.array(k1_pred).reshape(-1,1))*k1_pred)
+#print(k2_pred/scaler_x.inverse_transform(np.array(k2_pred).reshape(-1,1))*k2_pred)
 print(k1_pred)
-print(scaler_x.inverse_transform(np.array(k1_pred).reshape(-1,1)))
 print(k2_pred)
-print(scaler_x.inverse_transform(np.array(k2_pred).reshape(-1,1)))
 # R2-score
 print(r2_score(yA_pred, yA_true))
 print(r2_score(yB_pred, yB_true))
